3c5bd398fdf0549f353fd5aa72623c238aa10822
darius-despain <darius.despain@gmail.com>
2022-06-28 20:29:27 -0600

    removed login file, moving experimentation to new repo

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState} from 'react';
 import config from './config'
-import Login from './Login'
 
 const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
@@ -20,7 +19,6 @@ const App = () => {
     <div>
       App is running - good work:
       { names.map(author => author.firstName + " ")}
-      <Login />
     </div>
   );
 }
